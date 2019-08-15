cdca5b5bb41f9efc3739a2fc4429bdcecee23f5d
dmitriy <dmitriy.repka.jazzteam@gmail.com>
2019-08-15 09:41:53 +0300

    add

---
+++ test/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="wrapper">
+            <div className="App">header</div>
+            <textarea className="textarea"/>
+            <div className="big_block"/>
+        </div>
+    )
+}
+
+export default App;
